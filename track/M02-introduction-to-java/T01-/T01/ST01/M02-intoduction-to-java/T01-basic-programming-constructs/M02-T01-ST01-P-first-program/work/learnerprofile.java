3d5faf7945880e22d6c99c20e5a79229e0e86a9c
manigeethika <manigeethikareddy673@gmail.com>
2026-08-03 15:57:04 +0530

    day2

---
+++ track/M02-introduction-to-java/T01-/T01/ST01/M02-intoduction-to-java/T01-basic-programming-constructs/M02-T01-ST01-P-first-program/work/learnerprofile.java
@@ -0,0 +1,10 @@
+public class learnerprofile {
+
+    public static void main(String[] args) {
+        String learnerName = "Manigeethika";
+        int dailyPracticeHours = 7;
+
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
