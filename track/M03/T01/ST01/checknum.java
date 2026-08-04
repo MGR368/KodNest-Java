052f49249957450abbc8f30df3b817ae28d7f8c5
manigeethika <manigeethikareddy673@gmail.com>
2026-08-04 12:29:29 +0530

    day3P2

---
+++ track/M03/T01/ST01/checknum.java
@@ -0,0 +1,14 @@
+
+public class checknum {
+
+    public static void main(String[] args) {
+        int num = 13;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num == 0) {
+            System.out.println("number is 0");
+        } else {
+            System.out.println("negative");
+        }
+    }
+}
