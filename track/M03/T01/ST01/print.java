bff5fe800210154cb9778a35ad1cd2984db54bae
manigeethika <manigeethikareddy673@gmail.com>
2026-08-04 11:35:24 +0530

    day3P1

---
+++ track/M03/T01/ST01/print.java
@@ -0,0 +1,13 @@
+
+public class print {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("tuesday");
+        System.out.println("wednesday");
+        System.out.println("thursday");
+        System.out.print("friday");
+        System.out.println("saturday");
+        System.out.print("sunday");
+    }
+}
