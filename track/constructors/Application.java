858998e6b22cd0123a5acad0b14ec83884400042
manigeethika <manigeethikareddy673@gmail.com>
2026-08-14 11:41:30 +0530

    changes

---
+++ track/constructors/Application.java
@@ -0,0 +1,10 @@
+package track.constructors;
+
+public class Application {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.input("Raja", 20, 5.5);
+        s1.display();
+    }
+}
+
---
+++ track/constructors/version2.java
@@ -0,0 +1,26 @@
+package track.constructors;
+
+class Student {
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+public class version2 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Raja", 20, 5.5);
+        s1.display();
+    }
+}
